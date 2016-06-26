79071ab65d687b9140ea165f58704a01aadefe7a
lastromanticx <giladbarkan@live.com>
2016-06-26 05:51:09 -0400

    working..

--- shopping.js
+++ shopping.js
@@ -1,3 +1,5 @@
+var cart = []
+
 function total() {
   let t = 0
 
@@ -8,4 +10,55 @@ function total() {
   }
 
   return t
+}
+
+function getCart(){
+  return cart;
+}
+
+function setCart(arr){
+  cart = arr;
+}
+
+function viewCart(){
+  if (cart.length == 0){
+    console.log("Your shopping cart is empty.")
+  }
+
+  for (let i in cart){
+    var str = cart.map(i => Object.keys(i)[0] + ' at ' + i[Object.keys(i)[0]]).join(', ');
+    console.log(`In your cart you have ${str}.`)
+  }
+}
+
+function addToCart(item){
+  var price = Math.floor(Math.random() * 101);
+  cart.push({item: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
+}
+
+function removeFromCart(item_name){
+  var index = null;
+  cart.forEach(function(v,i){ 
+    if (v.hasOwnProperty(item_name))
+      index = i;
+  });
+
+  if (index === null){
+    console.log ("That item is not in your cart");
+  } else{ 
+    cart.splice(i,1);
+  }
+  
+  return cart;
+}
+
+function placeOrder(cc){
+  if (cc === undefined){
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log(`Your total cost is ${total()}, which will be charged to ${cc}.`);
+  }
+  cart = [];
 }
