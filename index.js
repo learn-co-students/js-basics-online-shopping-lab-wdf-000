e2c103366184aea34551d2cab41f1db4556a49e8
Flatiron School <ellis.paige.v@gmail.com>
2016-10-16 13:28:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +14,55 @@ function total() {
   }
 
   return t
+}
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100);
+  cart.push( { [item]: price } );
+  console.log(`${item} has been added to your cart.`);
+  return getCart();
+}
+
+function viewCart() {
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  }
+  else {
+    var arr = [];
+    var line = "In your cart, you have ";
+    for (let i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        arr.push(`${item} at $${cart[i][item]}`);
+      }
+      console.log(line + arr.join(", ") + ".");
+    }
+  }
+}
+
+function removeFromCart(unwanted_item) {
+  var present = false
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].hasOwnProperty(unwanted_item)) {
+      cart = cart.slice(0, i).concat(cart.slice(0 + 1));
+      present = true
+    }
+  }
+  if (present === false) {
+    console.log("That item is not in your cart.")
+  }
+  return cart
+}
+
+function placeOrder(cardNumber) {
+  if (cardNumber === undefined) {
+    console.log("We don't have a credit card on file for you to place your order.")
+  }
+  else {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    cart = []
+  }
 }
